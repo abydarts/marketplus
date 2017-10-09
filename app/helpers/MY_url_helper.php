55e5ed0fa195cd4f7bf0201cdbbe9ffe1bb65f6b
Abyan A.F <abyan.site@gmail.com>
2017-10-09 15:23:17 +0700

    Add files via upload

---
+++ app/helpers/MY_url_helper.php
@@ -0,0 +1,109 @@
+<?php
+
+function ssl_support()
+{
+	$CI =& get_instance();
+    return $CI->config->item('ssl_support');
+}
+
+if ( ! function_exists('force_ssl'))
+{
+	function force_ssl()
+	{
+		if (ssl_support() &&  (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == 'off'))
+		{
+			$CI =& get_instance();
+			$CI->config->config['base_url'] = str_replace('http://', 'https://', $CI->config->config['base_url']);
+			redirect($CI->uri->uri_string());
+		}
+	}
+}
+
+//thanks C4iO [PyroDEV]
+if ( ! function_exists('remove_ssl'))
+{
+	function remove_ssl()
+	{	
+		if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on')
+		{
+			$CI =& get_instance();
+			$CI->config->config['base_url'] = str_replace('https://', 'http://', $CI->config->config['base_url']);
+			
+			redirect($CI->uri->uri_string());
+		}
+	}
+}
+
+//function theme_url($uri)
+//{
+//	$CI =& get_instance();
+//	return $CI->config->base_url('app/themes/'.$CI->config->item('theme').'/'.$uri);
+//}
+
+//Dynamic theme
+	function theme_url($uri) {
+		$CI =& get_instance(); 	 	
+			$row = $CI->db->get_where('templates', array('status' => "1"))->row(); 								// getting theme records in helper 
+				if(count($row)!=0){
+					$template=$row->theme_name; //template name  	
+					} else {		
+					$template=$CI->config->item('theme'); //default template
+				} 
+		return $CI->config->base_url('app/themes/'.$template.'/'.$uri);	
+} 
+//Dynamic theme
+
+//to generate an image tag, set tag to true. you can also put a string in tag to generate the alt tag
+function theme_img($uri, $tag=false)
+{
+	if($tag)
+	{
+		return '<img src="'.theme_url('assets/img/'.$uri).'" alt="'.$tag.'">';
+	}
+	else
+	{
+		return theme_url('assets/img/'.$uri);
+	}
+	
+}
+
+function theme_images($uri, $tag=false)
+{
+	if($tag)
+	{
+		return '<img src="'.theme_url('assets/images/'.$uri).'" alt="'.$tag.'">';
+	}
+	else
+	{
+		return theme_url('assets/images/'.$uri);
+	}
+	
+}
+
+function theme_js($uri, $tag=false)
+{
+	if($tag)
+	{
+		return '<script type="text/javascript" src="'.theme_url('assets/js/'.$uri).'"></script>';
+	}
+	else
+	{
+		return theme_url('assets/js/'.$uri);
+	}
+}
+
+//you can fill the tag field in to spit out a link tag, setting tag to a string will fill in the media attribute
+function theme_css($uri, $tag=false)
+{
+	if($tag)
+	{
+		$media=false;
+		if(is_string($tag))
+		{
+			$media = 'media="'.$tag.'"';
+		}
+		return '<link href="'.theme_url('assets/css/'.$uri).'" type="text/css" rel="stylesheet" '.$media.'/>';
+	}
+	
+	return theme_url('assets/css/'.$uri);
+}
---
+++ app/helpers/formatting_helper.php
@@ -0,0 +1,95 @@
+<?php 
+function format_address($fields, $br=false)
+{
+	if(empty($fields))
+	{
+		return ;
+	}
+	
+	// Default format
+	$default = "{firstname} {lastname}\n{company}\n{address_1}\n{address_2}\n{city}, {zone} {postcode}\n{country}";
+	
+	// Fetch country record to determine which format to use
+	$CI = &get_instance();
+	$CI->load->model('location_model');
+	$c_data = $CI->location_model->get_country($fields['country_id']);
+	
+	if(empty($c_data->address_format))
+	{
+		$formatted	= $default;
+	} else {
+		$formatted	= $c_data->address_format;
+	}
+
+	$formatted		= str_replace('{firstname}', $fields['firstname'], $formatted);
+	$formatted		= str_replace('{lastname}',  $fields['lastname'], $formatted);
+	$formatted		= str_replace('{company}',  $fields['company'], $formatted);
+	
+	$formatted		= str_replace('{address_1}', $fields['address1'], $formatted);
+	$formatted		= str_replace('{address_2}', $fields['address2'], $formatted);
+	$formatted		= str_replace('{city}', $fields['city'], $formatted);
+	$formatted		= str_replace('{zone}', $fields['zone'], $formatted);
+	$formatted		= str_replace('{postcode}', $fields['zip'], $formatted);
+	$formatted		= str_replace('{country}', $fields['country'], $formatted);
+	
+	// remove any extra new lines resulting from blank company or address line
+	$formatted		= preg_replace('`[\r\n]+`',"\n",$formatted);
+	if($br)
+	{
+		$formatted	= nl2br($formatted);
+	}
+	return $formatted;
+	
+}
+
+function format_currency($value, $symbol=true)
+{
+	if(!is_numeric($value))
+	{
+		return;
+	}
+	
+	$CI = &get_instance();
+	
+	if($value < 0 )
+	{
+		$neg = '- ';
+	} else {
+		$neg = '';
+	}
+	
+	$symbol= $CI->auth->symbol($CI->auth->currency_select('currency'));
+			   
+		   if($symbol)	{
+				
+			$convert_value= currency_convert($CI->auth->currency_select('currency'),$CI->auth->currency_select('currency'),$value); 
+			
+			$formatted	= number_format(abs($convert_value), 2, $CI->config->item('currency_decimal'), $CI->config->item('currency_thousands_separator'));	
+			
+			if($CI->config->item('currency_symbol_side') == 'left') {
+				$formatted	= $neg.$symbol.$formatted;
+				} else {
+				$formatted	= $neg.$symbol;
+				}
+			} else {		
+				$formatted	= number_format(abs($convert_value), 2, '.', ','); 	
+		}		
+		return $formatted.' '.$CI->auth->currency_select('currency');;
+}
+function currency_convert($from,$to,$amount) {
+		
+		if($from!=$to){
+	  $amounts = urlencode(1);
+  $from_Currency = urlencode($from);
+  $to_Currency = urlencode($to);
+  $get = file_get_contents("https://www.google.com/finance/converter?a=$amounts&from=$from_Currency&to=$to_Currency");
+  $get = explode("<span class=bld>",$get);
+  $get = explode("</span>",$get[1]);  
+  $converted_amount = preg_replace("/[^0-9\.]/", null, $get[0]);
+ 
+  return $converted_amount*$amount;
+		}
+		else{
+			return $amount;
+			}
+}
